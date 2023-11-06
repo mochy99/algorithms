f4b67fb5367a692d863f1dc779e7d12dc1521196
mochy99 <mochynguyen99@gmail.com>
2023-11-06 00:18:04 -0800

    heap algorithm

---
+++ heap.py
@@ -0,0 +1,27 @@
+#input: un ordered array
+#ouput: heap array (min and/or max) *min
+array = [9,8,7,6,5,4,3,2,1]
+def heapify(array):
+    n = len(array)
+    if n > 1:
+        self = n - 1
+        parent = ((self + 1)// 2) - 1
+        
+        while array[self] < array[parent]:
+            array[self], array[parent] = array[parent], array[self]
+            if parent != 0:
+                self = parent
+                parent = ((self + 1)// 2) - 1
+           
+            
+
+def heap(array):
+    n = len(array)
+    heap = []
+    for i in range(n):
+        heap.append(array[i])
+        heapify(heap)
+        
+    return heap
+
+print(heap(array))
