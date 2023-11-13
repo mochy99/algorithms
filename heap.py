9e1dc2115d30b080890e4d54025927db622f0853
mochy99 <mochynguyen99@gmail.com>
2023-11-13 00:57:16 -0800

    added high and low heapify

--- heap.py
+++ heap.py
@@ -1,13 +1,25 @@
 #input: un ordered array
 #ouput: heap array (min and/or max) *min
 array = [9,8,7,6,5,4,3,2,1]
-def heapify(array):
+def low_heapify(array):
     n = len(array)
     if n > 1:
         self = n - 1
         parent = ((self + 1)// 2) - 1
         
         while array[self] < array[parent]:
+            array[self], array[parent] = array[parent], array[self]
+            if parent != 0:
+                self = parent
+                parent = ((self + 1)// 2) - 1
+
+def high_heapify(array):
+    n = len(array)
+    if n > 1:
+        self = n - 1
+        parent = ((self + 1)// 2) - 1
+        
+        while array[self] > array[parent]:
             array[self], array[parent] = array[parent], array[self]
             if parent != 0:
                 self = parent
@@ -20,8 +32,7 @@ def heap(array):
     heap = []
     for i in range(n):
         heap.append(array[i])
-        heapify(heap)
+        low_heapify(heap)
         
     return heap
 
-print(heap(array))
