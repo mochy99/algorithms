1786a1ff8976d2013d1676058fd2b0e925eadb3d
mochy99 <mochynguyen99@gmail.com>
2023-11-13 01:20:04 -0800

    test function

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-897633249888622
-897633249888622
-897633249888622
-897633249888622
-897633249888622
-897633249888622
-89763324988622
+def test(order, input, output):
+    expect_output = input
+    if expect_output == output:
+        return "Test " + str(order) + " passed"
+    else:
+        return "Test " + str(order) + " failed"
