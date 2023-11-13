e889ea6b69dd7ad52367e80aa97c93eb8e1a63d2
mochy99 <mochynguyen99@gmail.com>
2023-11-13 13:02:59 -0800

    merged into heap.py

--- extractMin.py
+++ extractMin.py
@@ -17,6 +17,21 @@ def extractMin(arr):
             self = r
         l = (self + 1) * 2 - 1
         r = (self + 1) * 2 
-    print(heap)
     return result
-print(extractMin(heap))
+def extractMax(arr):
+    result = arr[0]
+    arr[0] = arr.pop(len(arr)-1)
+    n = len(arr)
+    self = 0
+    l = (self + 1) * 2 - 1
+    r = (self + 1) * 2
+    while (l < n and r < n and (arr[self] < arr[l] or arr[self] < arr[r])):
+        if arr[l] > arr[r]:
+            arr[self], arr[l] = arr[l], arr[self]
+            self = l
+        else:
+            arr[self], arr[r] = arr[r], arr[self]
+            self = r
+        l = (self + 1) * 2 - 1
+        r = (self + 1) * 2 
+    return result
