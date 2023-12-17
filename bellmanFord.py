a955475912956048eaf6dccd509a70cd95c98707
mochy99 <mochynguyen99@gmail.com>
2023-12-17 02:01:52 -0800

    passed 2 test cases'

--- bellmanFord.py
+++ bellmanFord.py
@@ -1,7 +1,40 @@
-from convertArray import convertSetEdges
-
+from convertArray import convertDirectedGraph
+import sys
 def bellmanFord(fileName):
-    setVetices, setEdges, setWeight = convertSetEdges(fileName)
-    print(setVetices)
+    setVetices, setEdges = convertDirectedGraph(fileName)
+    budget = len(setVetices)
+    result = sys.maxsize
+    for start in setVetices:
+        init, prev = [None], set()
+        # Initialize 
+        prev.add(start)
+        for vertex in setVetices:
+            if vertex == start:
+                init.append(0) 
+            else:
+                init.append(sys.maxsize) 
+        # Main loop
+        for i in range(budget):
+            new_nodes = set()
+            for tail in prev:
+                if tail in setEdges:
+                    for head, weight in setEdges[tail]:
+                        value = min(init[head], init[tail] + weight)
+                        init[head] = value
+                        result = min(result, value)
+                        new_nodes.add(head)
+            prev.update(new_nodes)
+        # Detect a negative cycle
+        for tail in prev:
+            if tail in setEdges:
+                for head, weight in setEdges[tail]:
+                    value = min(init[head], init[tail] + weight)
+                    if init[head] != value:
+                        return "A negative cycle"
+                        
+    return result
+
+
+    
 
 print(bellmanFord("problem_set/course4/g1week1.txt"))
