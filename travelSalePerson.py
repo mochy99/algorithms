cccba4f2fce94a7c0d0dbd1a53c056cfeb7efbf8
mochy99 <mochynguyen99@gmail.com>
2024-01-01 00:56:28 -0800

    tracking path

--- travelSalePerson.py
+++ travelSalePerson.py
@@ -3,19 +3,36 @@ import math
 import sys
 
 def tsp(fileName):
+    # Declare variables
     setVertices, data = convertTsp(fileName)
+    lastRound = ''
+    result = sys.maxsize
+    n = len(data)
+    subset = {}
+    prev = {}
+    for size in range(2, n+1):
+        subset[size] = []
+
+    # Helper function
     def Euclidean(i,j):
         xi, yi = data[i]
         xj, yj = data[j]
         return math.sqrt((xj - xi)**2 + (yj - yi)**2)
-    n = len(data)
-    subset = {}
-    prev = [None] * (n+1)
-    for size in range(2, n+1):
-        subset[size] = []
+    def toString(subSet):
+        string = ''
+        for item in subSet:
+            string += str(item)
+        return string
+    def toStringNotIncludedJ(subSet, j):
+        string = ''
+        for item in subSet:
+            if item != j:
+                string += str(item)
+        return string
+   
     
     # Generate all subsets having size greater than 1
-    for i in range(2**n):
+    for i in range(2**n + 1):
         current_subset = set()  
         for j in range(n):
             if (i & (1 << j)) > 0:
@@ -28,32 +45,45 @@ def tsp(fileName):
     # Assign value for base case
     for curSet in  subset[2]:
         i,j = curSet
+        curSet = toString(curSet)
         distance =  Euclidean(i,j)
+        prev[curSet] = [sys.maxsize] * (n+1)
         if i == 1:
-            prev[j] = distance
+            prev[curSet][j] = (distance, j)
         else:
-            prev[i] = distance
-    print(prev)
+            prev[curSet][i] = (distance, j)
+    
     # Systematically solve all subproblems
-    for size in range(3, n):
-        tem = [None] * (n+1)
+    for size in range(3, n+1):
+        tem = {}
         for curSet in subset[size]:
+            if size == n:
+                lastRound = toString(curSet)
+            tem[toString(curSet)] = [sys.maxsize] * (n+1)
             for j in curSet:
                 if j != 1:
                     curMin = sys.maxsize
                     for k in curSet:
                         if k != 1 and k != j:
-                            if j not in curSet:
-                                curMin = min(curMin, 1)
-                            
+                            subSetNotj = toStringNotIncludedJ(curSet, j)
+                            distance, subset = prev[subSetNotj][k]
+                            curMin = min(curMin, distance + Euclidean(k,j))
+                            newPath = subset = str(k)
+                            tem[toString(curSet)][j] = (curMin, newPath)
+        prev = {}
+        prev = tem
+    tour = ''
+    for i in range(2, len(prev[lastRound])):
+        value, path = prev[lastRound][i]
+        if value + Euclidean(1,i) < result:
+            result = value + Euclidean(1,i)
+            tour = path
 
     
+    return result, tour
 
+                            
 
-
-
-
-    return subset
 
 
 # print(tsp('problem_set/course4/tsp.txt'))
