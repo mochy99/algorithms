d04b5ff46c5cdfa34540760ddd8867b673da7995
mochy99 <mochynguyen99@gmail.com>
2024-01-10 20:53:42 -0800

    answers

--- DFS.py
+++ DFS.py
@@ -1,117 +1,86 @@
-graphA = [
-    ["s","a"],
-    ["s","b"],
-    ["a","c"],
-    ["b","c"],
-]
-graphB = [
-    ["A", "B"],
-    ["A", "C"],
-    ["B", "D"],
-    ["C", "D"],
-    ["C", "E"],
-    ["D", "E"],
-    ["E", "F"],
-    ["E", "G"],
-    ["F", "G"],
-    ["G", "H"],
-]
-graphC = [
-    ["A", "B"],
-    ["B", "D"],
-    ["B", "C"],
-    
-    ["C", "A"],
-    ["C", "L"],
-    ["C", "N"],
-    ["D", "E"],
-    ["D", "F"],
-    ["E", "G"],
-    ["F", "E"],
-    ["G", "F"],
-    ["K", "G"],
-    ["K","M"],
-    ["L","F"],
-    ["L", "K"],
-    ["L", "M"],
-    ["M", "N"],
-    ["N", "L"]
-]
-graphD = [
-    [1,7],
-    [7,4],
-    [7,9],
-    [4,1],
-    [6,8],
-    [9,6],
-    [6,3],
-    [3,9],
-    [5,8],
-    [2,5],
-    [8,2]
-]
+import sys, threading
+sys.setrecursionlimit(800000)
+threading.stack_size(67108864)
+def main():
+    def convertSCC(fileName):
+        with open(fileName, 'r') as file:
+            lines = file.readlines()
+        data = []
+        for line in lines:
+            i,j = map(int, line.split())
+            data.append([i,j])
+        return data
 
-def listNode(graph, start):
-    listNode = [start]
-    listPointer = {}
-    for edge in graph:
-        if edge[0] not in listNode:
-            listNode.append(edge[0])
-            
-        if edge[1] not in listNode:
-            listNode.append(edge[1])
+    def listNode(graph, start):
+        listNode = [start]
+        listPointer = {}
+        for edge in graph:
+            if edge[0] not in listNode:
+                listNode.append(edge[0])
+                
+            if edge[1] not in listNode:
+                listNode.append(edge[1])
 
-        if edge[0] in listPointer:
-            listPointer[edge[0]].append(edge[1]) 
-        else:
-            listPointer[edge[0]]= [edge[1]]
+            if edge[0] in listPointer:
+                listPointer[edge[0]].append(edge[1]) 
+            else:
+                listPointer[edge[0]]= [edge[1]]
 
-    return listNode, listPointer
+        return listNode, listPointer
 
+    def topologicalSort(graph, start):
+        nodes, outgoing = listNode(graph, start)
+        explored = set()
+        order = []
 
-
-def topologicalSort(graph, start):
-    nodes, outgoing = listNode(graph, start)
-    explored = set()
-    order = []
-
-    def dfs (graph, start):
-        explored.add(start)
-        if start in outgoing:
-            for frontier in outgoing[start]:
-                if frontier not in explored:
-                    dfs(graph, frontier)
-        if start not in order:
-            order.append(start)
-    
-    for vertex in nodes:
-        if vertex not in explored:
-            dfs(graph, vertex)           
-    return order
-
-def kosaraju(graph,start):
-    graphRev = []
-    for edge in graph:
-        graphRev.append([edge[1], edge[0]])
-    nodes, outgoing = listNode(graph, start)
-    rev = topologicalSort(graphRev,start)
-    print(rev)
-    explored = []
-    numSCC = 0
-    def dfsSCC (graph, start):
-        explored.append(start)
+        def dfs (graph, start):
+            explored.add(start)
+            if start in outgoing:
+                for frontier in outgoing[start]:
+                    if frontier not in explored:
+                        dfs(graph, frontier)
+            if start not in order:
+                order.append(start)
         
-        if start in outgoing:
-            for vertex in outgoing[start]:
-                if vertex not in explored:
-                    dfsSCC(graph, vertex)
+        for vertex in nodes:
+            if vertex not in explored:
+                dfs(graph, vertex)           
+        return order
 
-    for i in range(len(rev) - 1, -1, -1):
-        vertex = rev[i]
-        if vertex not in explored:
-            numSCC += 1
-            dfsSCC(graph, vertex)
-            print(explored)
-    return numSCC      
+    def kosaraju(fileName,start):
+        graph= convertSCC(fileName)
+        graphRev = []
+        for edge in graph:
+            graphRev.append([edge[1], edge[0]])
+        nodes, outgoing = listNode(graph, start)
+        rev = topologicalSort(graphRev,start)
 
-print(kosaraju(graphD, 1))
+        explored = []
+        numSCC = 0
+        result = [0]
+        def dfsSCC (graph, start):
+            explored.append(start)
+            
+            if start in outgoing:
+                for vertex in outgoing[start]:
+                    if vertex not in explored:
+                        dfsSCC(graph, vertex)
+
+        for i in range(len(rev) - 1, -1, -1):
+            vertex = rev[i]
+            oldSize = len(explored)
+            if vertex not in explored:
+                numSCC += 1 
+                dfsSCC(graph, vertex)
+                newSize = len(explored) -oldSize
+                result.append(newSize)
+        result.sort()
+        return result      
+
+
+    print(kosaraju('problem_set/course2/scc.txt', 1))
+
+thread = threading.Thread(target=main)
+thread.start()
+
+
